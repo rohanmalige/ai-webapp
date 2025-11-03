f78a3a4daae2cc0be8b0c4505d86d330c8532433
rohanmalige <rmmalige@ucdavis.edu>
2025-11-03 16:22:26 -0600

    Added dynamic background along with a palceholder box

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
-export default function App() {
+//I used reactbits to get my dynamic background https://reactbits.dev/backgrounds/color-bends
+import ColorBends from "./components/ColorBends";
+import "./index.css";
+
+function App() {
   return (
-    <div>
-      <h1>AI Web App 🤖</h1>
-      <p>Start building your chatbot here!</p>
+    <div className="app-container">
+      {/* Background */}
+      <div className="Background">
+        <ColorBends
+          colors={["#ff5c7a", "#ffff14ff", "#00fff7ff","#001affff","#00ff44ff"]}
+          rotation={45}
+          speed={0.3}
+          scale={1.2}
+          frequency={1.4}
+          warpStrength={1}
+          mouseInfluence={0.8}
+          parallax={0.6}
+          noise={0.08}
+        />
+      </div>
+
+      {/* Centered box */}
+      <div className="app-content">
+        <h1>Welcome </h1>
+        <p style={{ textAlign: "center", color: "#333" }}>
+          layout with dynamic background. pretty cool
+        </p>
+      </div>
     </div>
   );
 }
+
+export default App;
