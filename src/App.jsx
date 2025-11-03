8ef756d23ebfc4a8733a314afbfd6d3779f7272c
rohanmalige <rmmalige@ucdavis.edu>
2025-11-03 17:48:37 -0600

    added text and two buttons to navigate to ai chat

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 //I used reactbits to get my dynamic background https://reactbits.dev/backgrounds/color-bends
-import ColorBends from "./components/ColorBends";
 import "./index.css";
-
+import ColorBends from "./components/ColorBends";
+import TextType from './components/TextType';
 function App() {
   return (
     <div className="app-container">
-      {/* Background */}
+      {/* Animated background */}
       <div className="Background">
         <ColorBends
-          colors={["#ff5c7a", "#ffff14ff", "#00fff7ff","#001affff","#00ff44ff"]}
+          colors={["#ff5c7a", "#ffff14ff", "#0044ffff"]}
           rotation={45}
           speed={0.3}
           scale={1.2}
@@ -20,12 +20,20 @@ function App() {
         />
       </div>
 
-      {/* Centered box */}
+      {/* Centered content */}
       <div className="app-content">
-        <h1>Welcome </h1>
-        <p style={{ textAlign: "center", color: "#333" }}>
-          layout with dynamic background. pretty cool
-        </p>
+        <TextType 
+          text={["Chat with our AI assistant."]}
+          typingSpeed={75}
+          pauseDuration={1500}
+          showCursor={true}
+          cursorCharacter="|"
+        />
+
+        <div className="button-group">
+          <button className="hover-border">Chat Now</button>
+          <button className="hover-border dark">Learn More</button>
+        </div>
       </div>
     </div>
   );
